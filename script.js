a1ebb298d9f735d39600b78f7530f0dcf1ae33a7
xxxhdpi <xxxhdpi@naver.com>
2020-05-11 03:13:29 +0900

    유튜브보면서 연습

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ $(".event-popup").on("click", function () {
 $(".popup").on("click", function () {
     $(".popup").hide();
 })
+
+$(".v").html("About Us").css("color","red");
+
+/*
+$(document).ready(function () {
+    $('h1').css('color','red');
+    $('h1').css('background','yellow');
+})
+*/
+
+/*
+$(function () {
+    $('h1').css('color','red');
+});
+*/
+
+
+/*
+$(function () {
+    $('h1').css({
+        'color':'blue',
+        'background':'black',
+        'textdecoration':'underline'
+    })
+});
+*/
+
+$('h1').html('제이쿼리 연습').css({
+   'color':'#fff',
+   'background':'red'
+});
+
