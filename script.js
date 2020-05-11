51dcb840cd7f1d366b7be98cb5fd67683108ec70
xxxhdpi <xxxhdpi@naver.com>
2020-05-11 23:35:53 +0900

    연습중

--- script.js
+++ script.js
@@ -45,12 +45,12 @@ $(function () {
 });
 */
 
-$('h1').html('제이쿼리 연습').css({
+$('h1').html('여기를 클릭하면 회색박스가 없어진다우').css({
    'color':'#fff',
    'background':'red'
 });
 
-$('h3').html('이소연').css({
+$('h3').html('이소연의 제이쿼리 연습장').css({
     color: 'blue',
     fontsize: '72px'
 });
@@ -78,5 +78,30 @@ $('h1').on('click', function () {
 });
 
 $('.box02').on('click', function () {
-    $('.box03').animate({'opacity':0},3000);
+    $('.box03').animate({'opacity': 0},1500,
+        function () {
+            $('.box03').animate({'opacity':1},1500);
+        });
+
+});
+
+$('.section-list ul li').on('mouseover', function () {
+    $(this).css({'background-color':'#ddd'});
+})
+
+$('.section-list ul li').on('mouseout', function () {
+    $(this).css({'background-color':"#fff"});
 } );
+
+$('.btn-menu').on('click', function () {
+    $('ul').show();
+});
+
+$('.box04').on('click', function() {
+    $(this).animate({'margin-left': '200px'})
+});
+
+$('.box05').animate({'width':'700px'},
+    function () {
+        $(this).animate({'width':'100px'})
+} )
